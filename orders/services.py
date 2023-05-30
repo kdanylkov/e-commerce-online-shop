9346e820a46b5da99300ee2435b0d41295bfcc20
Kirill Danilkov <k.danilkov.work@gmail.com>
2023-05-30 15:53:48 +0300

    upd:order_create and payment_process

--- orders/services.py
+++ orders/services.py
@@ -8,7 +8,11 @@ from cart.cart import Cart
 @atomic
 def create_order_in_db(form: OrderCreationForm, cart: Cart) -> Order:
     if form.is_valid():
-        order: Order = form.save()
+        order: Order = form.save(commit=False)
+        if cart.coupon:
+            order.coupon = cart.coupon
+            order.discount = cart.coupon.discount
+        order.save()
 
         for item in cart:
             OrderItem.objects.create(order=order,
