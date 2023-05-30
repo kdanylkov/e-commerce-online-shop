8f4551fe1b7d305125e5544642170a6aaac50bed
Kirill Danilkov <k.danilkov.work@gmail.com>
2023-05-30 11:01:43 +0300

    add:coupon admin

--- coupons/models.py
+++ coupons/models.py
@@ -7,7 +7,8 @@ class Coupon(models.Model):
     valid_from = models.DateTimeField()
     valid_to = models.DateTimeField()
     discount = models.IntegerField(
-            validators=[MinValueValidator(0), MaxValueValidator(100)]
+            validators=[MinValueValidator(0), MaxValueValidator(100)],
+            help_text='Percentage value (0 to 100)'
             )
     active = models.BooleanField()
 
